c1f6a7b88f1f3911fd0131cb54698a3acc3d6c33
Ing.andres <camilopez200807@gmail.com>
2026-05-15 21:42:22 -0500

    Creé la comunicación entre ProductController y ProductService

--- src/main/java/com/pos/controller/ProductController.java
+++ src/main/java/com/pos/controller/ProductController.java
@@ -3,11 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.pos.controller;
+//Se importa la clase producto del paquete model
+import com.pos.model.Producto;
+//Se importa el servicio donde se encuentran las validaciones de los productos
+import com.pos.service.ProductService;
 
-/**
- *
- * @author pc
- */
 public class ProductController {
+    private ProductService service;
+
+    //Constructor con su respectivo parametro
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
     
+    public void createProduct(Producto producto){
+        //Dato de tipo boolean para validar producto
+        boolean valid = service.validateProduct(producto);
+        
+        //se verifican resultados a traves de un if/else
+        
+        if(valid)
+            System.out.println("Producto valido");
+        
+        else
+            System.out.println("Producto invalido");
+    }  
 }
