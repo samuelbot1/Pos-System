c92d3cbe738487ccb35333f473ebf49d2e2a10ba
samuelbot1 <samuelpolo0202@gmail.com>
2026-05-15 20:47:39 -0500

    conectar el main con el databaseconfig.

--- src/main/java/com/pos/app/PosSystemm.java
+++ src/main/java/com/pos/app/PosSystemm.java
@@ -4,6 +4,7 @@
 
 package com.pos.app;
 import com.pos.model.Producto;
+import com.pos.config.DatabaseConfig;
 
 /**
  *
@@ -13,9 +14,13 @@ public class PosSystemm {
 
     public static void main(String[] args) {
         
+        DatabaseConfig.getConnection();
+        
         Producto P = new Producto("Samuel" , 200 , 10);
         
         System.out.println(P);
+        
+        
         
     }
 }
